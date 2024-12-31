8964626e5a3a00d643db13585489fe48e45174b5
Houssam Hamouich <itshoussameddine@gmail.com>
2024-12-31 23:51:56 +0100

    feat: add second increment functionality to clock reducer; update initial state and action creators

--- src/components/actions/actions.js
+++ src/components/actions/actions.js
@@ -1,4 +1,5 @@
 export const increment_hour = () => ({type: 'INCREMENT_HOUR'})
-    
 
 export const increment_minute = () => ({type: 'INCREMENT_MINUTE'})
+
+export const increment_second = () => ({type: 'INCREMENT_SECOND'})
--- src/components/feature/clockReducer.js
+++ src/components/feature/clockReducer.js
@@ -1,5 +1,5 @@
 
-const initialState = {hour: new Date().getHours(), minute: new Date().getMinutes()}
+const initialState = {hour: new Date().getHours(), minute: new Date().getMinutes(), second: new Date().getSeconds()}
 
 const clockReducer = (state = initialState, action) => {
 
@@ -21,6 +21,18 @@ const clockReducer = (state = initialState, action) => {
                 return {...state, minute: 0, hour: state.hour + 1}
             }
             return {...state, minute: state.minute + 1}
+
+        case 'INCREMENT_SECOND':
+            if(state.hour >= 23 && state.minute >= 59 && state.second >= 59){
+                return {hour: 0, minute: 0, second: 0}
+            }
+            if(state.minute >= 59 && state.second >= 59){
+                return {hour: state.hour + 1, minute: 0}
+            }
+            if(state.second >= 59){
+                return {...state, minute: state.minute + 1 , second: 0}
+            }
+            return {...state, second: state.second + 1}
         
         default:
             return state;
